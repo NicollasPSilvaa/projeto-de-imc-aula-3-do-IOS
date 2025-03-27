220c6b3472077c330a484b753d0c1c7b2fc1d8e7
NicollasPSilvaa <nicollaspsilvaa@outlook.com>
2025-03-27 16:58:48 -0300

    outra versão

--- exemplo.js
+++ exemplo.js
@@ -1,15 +1,15 @@
-let altura = 1.69
-let peso = 74
+let altura = 1.62
+let peso = 44
 let imc = peso / (altura * altura);
 
 if (imc < 17) {
-    console.log('Você está muito abaixo do peso');
+    console.log('Você está muito abaixo do peso, seu imc ideal seria entre 18.5 e 24.99');
 } else if (imc > 17 && imc <= 18.49) {
-    console.log('você está abaixo do peso' );
+    console.log('você está abaixo do peso, o seu imc ideal seria entre 18.5 e 24.99' );
     } else if (imc >= 18.5 && imc <= 24.99) {
        console.log('você esta no peso normal' );
     } else if (imc >= 25 && imc <= 29.99) {
-       console.log('Você está acima do peso');
+       console.log('Você está acima do peso, o seu imc ideal seria entre 18.5 e 24.99');
       } else if (imc >= 30 && imc <= 34.99) {
         console.log('Você está na Obesidade procure ajuda')
       } else 
